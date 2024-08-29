31f9db6711af4526eb4a019825630618859a25a9
SerJv21 <serjv@live.ru>
2024-08-29 16:19:20 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,12 @@
+immutable_var = (1, 2, 'a', True)
+print(immutable_var)
+# immutable_var[3] = False
+# print(immutable_var)
+
+mutable_list = ["car", "call", " long"]
+print(mutable_list)
+mutable_list.append(100)
+mutable_list[2] = "Map"
+mutable_list[1] = "look"
+mutable_list.remove("look")
+print(mutable_list)
